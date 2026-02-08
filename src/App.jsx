73fab45d0e81a82d6c9e7a0883eb10dec516b650
ifX-Dev <ifxdev03@gmal.com>
2026-02-08 23:59:01 +0100

    image and text added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import product_mobile from "./assets/image-product-mobile.jpg"
 
 function App() {
   
@@ -6,8 +7,18 @@ function App() {
   return (
     <>
       <div>
-        <div></div>
-       </div>
+        <img src={product_mobile} alt="" />
+        <div>
+          <p>PERFUME</p>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+        <p>A floral, solar and voluptuous interpretation composed by Oliver Polge, Perfumer-Creator for the House of CHANEL</p>
+        <div>
+          <h2>$149.99</h2>
+          <p>$169.99</p>
+        </div>
+        </div>
+        <button></button>
+      </div>
     </>
   )
 }
